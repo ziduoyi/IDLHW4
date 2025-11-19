f17131a382111d4cca8bf1869c0cb93e08f9e1f0
Ziduo Yi <ziduoyi@gmail.com>
2025-11-19 01:37:29 -0500

    smal

--- hw4lib/trainers/lm_trainer.py
+++ hw4lib/trainers/lm_trainer.py
@@ -84,8 +84,8 @@ class LMTrainer(BaseTrainer):
             targets_golden = targets_golden.to(self.device)
             lengths = lengths.to(self.device)
         
-
-            with torch.autocast(device_type=self.device, dtype=torch.float16):
+            str = "cuda" if self.device.type == "cuda" else "cpu"
+            with torch.autocast(device_type=str, dtype=torch.float16):
 
                 # TODO: Get raw logits and attention weights from model
                 raw_preds, attn_weights = self.model(targets_shifted, target_lengths=lengths)
