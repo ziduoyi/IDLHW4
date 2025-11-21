17513b0acee3b1a51011ddc104019719cddf4056
Ziduo Yi <ziduoyi@gmail.com>
2025-11-21 12:22:02 -0500

    t

--- hw4lib/trainers/asr_trainer.py
+++ hw4lib/trainers/asr_trainer.py
@@ -9,8 +9,10 @@ from ..utils import create_scheduler, create_optimizer
 from ..model import DecoderOnlyTransformer
 import torchaudio.functional as aF
 import json
+import torchmetrics.text as tmt
 from torch.utils.data import Subset
 import pandas as pd
+
 
 
 class ASRTrainer(BaseTrainer):
